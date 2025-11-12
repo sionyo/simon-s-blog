7134bb1912287e226dd4ae194529cc841f1f9701
sionyo <simonyohannes251@gmail.com>
2025-11-12 14:19:50 +0300

    ''

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,17 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import './index.css'
+import App from './App.jsx'
 
-console.log('React is loading...') // Debug line
-
-try {
-  ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-  )
-  console.log('React mounted successfully') // Debug line
-} catch (error) {
-  console.error('React mount error:', error) // Debug line
-}
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
