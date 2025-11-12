825874a9bfc24fec314ea978a4b632c03db62169
sionyo <simonyohannes251@gmail.com>
2025-11-12 14:57:17 +0300

    ''

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ authController.initializeAdmin();
 
 app.use(cors({
   origin: process.env.NODE_ENV === 'production' 
-    ? ['https://simonsblog-six.vercel.app/'] 
+    ? ['https://simonsblog20.netlify.app/'] 
     : ['http://localhost:5173', 'http://127.0.0.1:5173'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
